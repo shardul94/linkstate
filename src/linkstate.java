f3584fb344f7b10ebbdebfe5b8578199a8ed58eb
Shardul Mahadik <shardulhere@gmail.com>
2014-04-07 21:30:47 +0530

    Initial Commit

---
+++ src/linkstate.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)linkstate.java
+ *
+ * linkstate Applet application
+ *
+ * @author 
+ * @version 1.00 2014/4/7
+ */
+ 
+import java.awt.*;
+import java.applet.*;
+
+public class linkstate extends Applet {
+	
+	public void init() {
+	}
+
+	public void paint(Graphics g) {
+		
+		g.drawString("Welcome to Java!!", 50, 60 );
+		
+	}
+}
