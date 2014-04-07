80411e419f994125a16a44336532ce954715c0aa
Shardul Mahadik <shardulhere@gmail.com>
2014-04-07 22:49:04 +0530

    Draw nodes Incomplete

---
+++ linkstate.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import java.applet.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+public class linkstate extends Applet{
+	public static ArrayList<Node> nodes;
+	public void init(){
+		try{
+			nodes = new ArrayList<Node>();
+			File input = new File("input.txt");
+			Scanner in = new Scanner(input);
+			int n = in.nextInt();
+			int e = in.nextInt();
+			for(int i=1;i<=n;i++)
+				nodes.add(new Node(in.nextInt(),in.nextInt()));
+				
+			new Thread(){
+				@Override
+				public void run(){
+					try{
+						repaint();
+						Thread.sleep(30);
+					}catch(Exception e){
+						System.out.println(e);
+					}
+				}
+			}.start();
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	public void paint(Graphics g){
+		for(Node n:nodes){
+			g.drawOval(n.xpos,n.ypos,30,30);
+			g.drawLine(n.xpos-21,n.ypos-21,n.xpos+21,n.ypos+21);
+		}
+		
+	}
+}
+class Node{
+	int xpos,ypos;
+	Node(int x, int y){
+		xpos = x;
+		ypos = y;
+	}
+}
+class Edge{
+	int n1,n2,cost,status;
+	Edge(int a, int b, int c, int s){
+		n1 = a;
+		n2 = b;
+		cost = c;
+		status = s;
+	}
+}
