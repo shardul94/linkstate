b6750d657c8cbc14d41258240c6751e962a8ee3a
Shardul Mahadik <shardulhere@gmail.com>
2014-04-08 09:25:59 +0530

    Draw Nodes Complete, Edge Class Added

--- linkstate.java
+++ linkstate.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.applet.*;
+//<applet code="linkstate.class" height=1000 width=1000> </applet>
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
@@ -32,8 +33,9 @@ public class linkstate extends Applet{
 	}
 	public void paint(Graphics g){
 		for(Node n:nodes){
-			g.drawOval(n.xpos,n.ypos,30,30);
-			g.drawLine(n.xpos-21,n.ypos-21,n.xpos+21,n.ypos+21);
+			g.drawOval(n.xpos,n.ypos,40,40);
+			g.drawLine(n.xpos+7,n.ypos+7,n.xpos+33,n.ypos+33);
+			g.drawLine(n.xpos+7,n.ypos+33,n.xpos+33,n.ypos+7);
 		}
 		
 	}
